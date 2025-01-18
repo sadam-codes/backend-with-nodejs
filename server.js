b1fb5be612f5c82399c70ff0b30c9f563983310b
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-01-18 21:23:58 +0500

    modules and lodash package

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const fs = require("fs");
 const os = require("os");
+const _ = require("lodash");
 
 const user = os.userInfo();
 console.log(user);
 console.log(user.username);
-fs.appendFile("greeting.text", "Aoa Sadam \n", () => console.log("file is created"));
+fs.appendFile("greeting.text", "Aoa Sadam \n", () =>
+  console.log("file is created")
+);
+
+const data = ["sadam", "sadam", 1, 1];
+var filter = _.uniq(data);
+console.log(filter);
+console.log(_.isArray(data));
