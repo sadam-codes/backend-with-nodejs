9d130c5fbd2b1d560ea6f90d95498d77f3d51e2a
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-02-27 23:46:00 +0500

    changes added

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,12 @@ const dbURL = 'mongodb://127.0.0.1:27017/hotels'
 mongoose.connect(dbURL)
 const db = mongoose.connection;
 db.on('connected', () => {
-    console.log("Connecetd database")
+    console.log("Connecetd to the database")
 })
 db.on('error', (error) => {
-    console.error("DisConnecetd database", error)
+    console.error("Error to the database", error)
 })
 db.on('disconnected', () => {
-    console.log("DisConnecetd database")
-}) 
+    console.log("DisConnecetd to the database")
+})
 module.exports = db
