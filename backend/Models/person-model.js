30feac733a9e5229270bf29e8ee38a2209b766eb
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-02-28 19:46:15 +0500

    models/Schema in node.js

---
+++ backend/Models/person-model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+// Schema
+const PersonSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is Required"]
+    },
+    age: {
+        type: Number,
+        required: [true, "Age is Required"],
+    },
+    role: {
+        type: String,
+        enum: ['chef', 'manager', 'waiter'], // Job role should be here
+        required: [true, "Role is Required"]
+    },
+    mobile: {
+        type: Number,
+        required: [true, "Mobile Number is Required"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is Required"],
+        unique: true
+    },
+    address: {
+        type: String,
+        required: [true, "Address is Required"],
+    },
+    salary: {
+        type: Number,
+      
+    }
+})
+// Model
+const person = mongoose.model('persons', PersonSchema);
+module.exports = person;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const db = require('./db')
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+const person = require('./Models/person-model')
+app.use(express.json());
 
 app.get("/aboutus", (req, res) => {
   const details = [
@@ -18,11 +16,26 @@ app.get("/aboutus", (req, res) => {
   res.send(details);
 });
 
-app.get("/contact", (req, res) => {
-  res.send("Contact Page");
-});
-app.post("/person", (req, res) => {
-  res.send("data is saved");
+// Save Person Data (POST)
+app.post("/person", async (req, res) => {
+  try {
+    const { name, age, role, mobile, email, address, salary } = req.body;
+
+    const newPerson = new person({
+      name: name,
+      age: age,
+      role: role,
+      mobile: mobile,
+      email: email,
+      address: address,
+      salary: salary,
+    });
+    await newPerson.save();
+    res.status(201).json({ message: "Data saved successfully", newPerson });
+
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 app.listen(3000, () => {
   const PORT = 3000;
