3c8883a6847867bde8bffbc37877095b4f65f69d
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-03-11 12:16:52 +0500

    mongodb atlas URL Added

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const dbURL = 'mongodb://127.0.0.1:27017/hotels'
+// const dbURL = 'mongodb://127.0.0.1:27017/hotels'
+const dbURL = 'mongodb+srv://sadammuneer:sadam12345@cluster0.07bagy7.mongodb.net/hotels'
 mongoose.connect(dbURL)
 const db = mongoose.connection;
 db.on('connected', () => {
